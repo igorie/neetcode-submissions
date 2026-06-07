0af6c645b1c1259235262c3b939ae45d520e04a3
Igor Ievgeniev <101573854+igorie@users.noreply.github.com>
2026-06-07 10:58:38 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.ts
@@ -0,0 +1,44 @@
+class Solution {
+    private readonly delimiter = '#';
+    
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let hash = '';
+
+        for (let str of strs) {
+            hash += `${str.length}${this.delimiter}${str}`;
+        }
+
+        return hash;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const strs = [];
+        
+        let cursor = 0;
+
+        while (cursor < str.length) {
+            let lengthCursor = cursor;
+
+            while (str[lengthCursor] !== this.delimiter) {
+                lengthCursor++;
+            }
+
+            let length = parseInt(str.substring(cursor, lengthCursor));
+            cursor = lengthCursor + 1;
+            lengthCursor = cursor + length;
+
+            strs.push(str.substring(cursor, lengthCursor));
+            cursor = lengthCursor;
+        }
+
+        return strs;
+    }
+}

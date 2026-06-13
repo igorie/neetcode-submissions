f09b67c8f0fbdd55cfc2c6b868bcb47dd1643cee
Igor Ievgeniev <101573854+igorie@users.noreply.github.com>
2026-06-13 11:39:28 +0200

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const closeBrackets = {
+            '}': '{',
+            ')': '(',
+            ']': '[',
+        };
+        const stack = [];
+
+        for (const char of s) {
+            const closeBracket = closeBrackets[char];
+
+            if (closeBracket) {
+                if (stack.length > 0 && stack[stack.length - 1] === closeBracket) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(char);
+            }
+
+            console.log(stack);
+        }
+
+        return !stack.length;
+    }
+}

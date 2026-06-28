808729ee713fa1a5e1ed315b7bc29a217394d926
Igor Ievgeniev <101573854+igorie@users.noreply.github.com>
2026-06-28 17:59:26 +0200

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        let node = head;
+        let index = 0;
+        const map = new WeakMap()
+
+        while (node) {
+            map.set(node, index);
+            node = node.next;
+            index++;
+
+            const hasCycle = map.has(node);
+
+            if (hasCycle) {
+                return hasCycle;
+            }
+        }
+
+        return false;
+    }
+}
